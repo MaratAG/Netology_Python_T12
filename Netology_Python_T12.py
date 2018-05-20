cca6b45ab3ef108e1f66cd29c09cef5a7872dd03
Marat Genatulin <marat@genatulin.com>
2018-05-20 19:32:53 +0300

    Внесены правки в решение по замечаниям преподавателя (от 16.04.2018).

--- Netology_Python_T12.py
+++ Netology_Python_T12.py
@@ -1,16 +1,24 @@
 """Программа по поиску фрагмента текста в наборе файлов."""
 import os
 
-import maratag as MG
+
+def find_word(search_file, word):
+    """Функция поиска слова в файле."""
+    find_word = False
+    if search_file.lower().count(word.lower()) > 0:
+        find_word = True
+    return find_word
 
 
 def find_files(files_dir, files, word):
     """Функция по поиску файлов, содержащих строку."""
     locate_files = list()
     for item in files:
-        if item[-3:].lower() == 'sql':
-            search_file = MG.Newsfile(os.path.join(files_dir, item), 'txt')
-            if search_file.find_word(word):
+        if item.endswith('.sql'):
+            filename = os.path.join(files_dir, item)
+            with open(filename, 'r') as f:
+                search_file = f.read()
+            if find_word(search_file, word):
                 locate_files.append(item)
     return locate_files
 
@@ -25,17 +33,19 @@ def print_locate_files(files):
 def main():
     """Инициализация выполнения программы."""
     word = None
-    files_dir = os.path.join(os.getcwd(), 'Migrations')
+    script_dir = __file__
+    end_of_path_dir = script_dir.rfind('/') + 1
+    files_dir = os.path.join(script_dir[:end_of_path_dir], 'Migrations')
     files = os.listdir(files_dir)
 
     while True:
-        word = input('Введите строку (слово ''выход'' - закончить):').strip()
-        if word == 'выход':
-            break
-        files = find_files(files_dir, files, word)
-        print_locate_files(files)
-        if len(files) <= 1:
-            break
+         word = input('Введите строку (слово ''выход'' - закончить):').strip()
+         if word == 'выход':
+             break
+         files = find_files(files_dir, files, word)
+         print_locate_files(files)
+         if len(files) <= 1:
+             break
 
 
 main()
